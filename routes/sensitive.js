bdf9b0d9b91555e0317c4f962352b8485f431d58
Kevin Wu <kevinwu098@gmail.com>
2023-11-11 18:22:51 -0800

    Correct sensitive id return

--- routes/sensitive.js
+++ routes/sensitive.js
@@ -4,26 +4,26 @@ const { db } = require('../server/db');
 const sensitiveRouter = express.Router();
 
 sensitiveRouter.get('/', async (req, res) => {
-    try {
-        const allSensitive = await db.query(`SELECT * from sensitive_data;`);
-        res.send(allSensitive);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err.message);
-    }
+  try {
+    const allSensitive = await db.query(`SELECT * from sensitive_data;`);
+    res.send(allSensitive);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
 });
 
 sensitiveRouter.get('/:id', async (req, res) => {
-    const sensitiveId = req.params.id;
+  const sensitiveId = req.params.id;
 
-    try {
-        const sensitives = await db.query(`SELECT * from sensitive_data WHERE id = ${sensitiveId};`);
+  try {
+    const sensitives = await db.query(`SELECT * from sensitive_data WHERE id = ${sensitiveId};`);
 
-        res.status(200).json(sensitives[0].quote_text);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err.message);
-    }
+    res.status(200).json(sensitives[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
 });
 
 module.exports = sensitiveRouter;
