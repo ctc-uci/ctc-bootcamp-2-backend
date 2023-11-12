72f79fbcf808a30329d257ef883e7eeb08b7aa08
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2023-11-11 17:32:30 -0800

    /sensitve

--- routes/sensitive.js
+++ routes/sensitive.js
@@ -5,22 +5,25 @@ const sensitiveRouter = express.Router();
 
 sensitiveRouter.get('/', async (req, res) => {
     try {
-      const allSensitive = await db.query(`SELECT * from sensitive_data;`);
-      res.send(allSensitive);
-  // const { keysToCamel } = require('../common/utils');
-  const { db } = require('../server/db');
+        const allSensitive = await db.query(`SELECT * from sensitive_data;`);
+        res.send(allSensitive);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err.message);
+    }
+});
 
 sensitiveRouter.get('/:id', async (req, res) => {
-  const sensitiveId = req.params.id;
+    const sensitiveId = req.params.id;
 
-  try {
-    const sensitives = await db.query(`SELECT * from sensitive_data WHERE id = ${sensitiveId};`);
+    try {
+        const sensitives = await db.query(`SELECT * from sensitive_data WHERE id = ${sensitiveId};`);
 
-    res.status(200).json(sensitives[0].quote_text);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err.message);
-  }
+        res.status(200).json(sensitives[0].quote_text);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err.message);
+    }
 });
 
 module.exports = sensitiveRouter;
